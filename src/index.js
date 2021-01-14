26343c03de4322b6b0c0280f80374e4d95790264
Pablo Vinicius <vinicius.pablo.18@gmail.com>
2021-01-14 11:22:17 -0300

    add sentry to track errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './App';
 import './static/styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+Sentry.init({
+  dsn: "https://6cb34312abaa465e8c65babf715120e9@o322920.ingest.sentry.io/5592749",
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
